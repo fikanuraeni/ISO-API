4c171d723b7228b367cd1afe1df650bfc9512e5d
Fika Nuraeni <fikanuraeni01810@gmail.com>
2022-07-19 13:26:19 +0700

    menambahkan athurization

--- src/server.js
+++ src/server.js
@@ -1,26 +1,105 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+const authentication = require('./api/authentication');
+const Database = require('./conf/Database');
+const AuthenticationService = require('./services/mysql/AuthenticationService');
+const AuthenticationValidator = require('./validator/authentication');
+
+const products = require('./api/products');
+const ProductsService = require('./services/mysql/ProductsService');
+const ProductsValidator = require('./validator/products');
+
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
+  const database = new Database();
+  const authenticationService = new AuthenticationService(database);
+  const productsService = new ProductsService(database);
 
-    const server = Hapi.server({
-      host: process.env.HOST,
-      port: process.env.PORT,
-      routes: {
-        cors: {
-          origin: ['*'],
-        },
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
       },
-    });
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: () => ({
-          name: 'Rski Mulud Muchamad',
-        }),
-      });
-      await server.start();
-  console.log(`Server running at ${server.info.uri}`);  
+    },
+  });
 
-  };
-  init ();
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      name: 'Rski Mulud Muchamad',
+    }),
+  });
+
+  // register external plugin
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+    // defines authentication strategy
+    server.auth.strategy('eshop_jwt', 'jwt',{
+      keys: process.env.TOKEN_KEY,
+      verify: {
+        aud: false,
+        iss: false,
+        sub: false,
+      },
+      validate: (artifacts) => ({
+        isValid: true,
+        credentials: {
+          id: artifacts.decoded.payload.id,
+        },
+      }),
+    });
+  // defines external plugin
+  await server.register([
+    {
+      plugin: authentication,
+      options: {
+        service: authenticationService,
+        validator: AuthenticationValidator,
+      },
+    },
+
+    {
+      plugin: products,
+      options: {
+        service: productsService,
+        validator: ProductsValidator,
+      }
+    },
+  ]);
+
+  // extension
+  server.ext('onPreResponse', (request, h) => {
+    const {
+      response
+    } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    console.log(response);
+
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server running at ${server.info.uri}`);
+
+};
+
+init();
